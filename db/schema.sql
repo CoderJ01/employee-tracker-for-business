2a269435fdf29fccf17f5085b5d5722e83f9d23b
Coder J <buildingguy@sbcglobal.net>
2022-02-05 19:16:08 -0800

    create placeholder values for tables

--- db/schema.sql
+++ db/schema.sql
@@ -3,13 +3,13 @@ DROP TABLE IF EXISTS position
 DROP TABLE IF EXISTS employee
 
 CREATE TABLE department (
-    id INT PRIMARY KEY
-    department_name VARCHAR(30)
+    id INT PRIMARY KEY,
+    name VARCHAR(30)
 );
 
 CREATE TABLE position (
     id INT PRIMARY KEY,
-    title VARCHAR(30)
+    title VARCHAR(30),
     salary DECIMAL,
     department_id INT
 );
@@ -18,5 +18,6 @@ CREATE TABLE employee (
     id INTEGER PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
+    role_id INT,
     manager_id INT
 );
