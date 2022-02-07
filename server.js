e1e816ba115c6ff674b1a04fcfe72b4d6b8682c2
Coder J <buildingguy@sbcglobal.net>
2022-02-06 18:16:43 -0800

    set up seconday questions for prompt

--- server.js
+++ server.js
@@ -13,37 +13,37 @@ var optionsList = () => {
     ])
     .then(
         answer => {
-            console.log(answer.tableChoice);
+            var choice = answer.tableChoice;
 
-            if (answer.tableChoice === 'View All Departments') {
+            if (choice === 'View All Departments') {
 
             }
 
-            if (answer.tableChoice === 'View All Roles') {
+            if (choice === 'View All Roles') {
                 
             }
 
-            if (answer.tableChoice === 'View All Employees') {
+            if (choice === 'View All Employees') {
                 
             }
 
-            if (answer.tableChoice === 'Add Department') {
-                
+            if (choice === 'Add Department') {
+                addDepartment();
             }
 
-            if (answer.tableChoice === 'Add Role') {
-                
+            if (choice === 'Add Role') {
+                addRole();
             }
 
-            if (answer.tableChoice === 'Add Employee') {
-                
+            if (choice === 'Add Employee') {
+                addEmployee();
             }
 
-            if (answer.tableChoice === 'Update Employee Role') {
-                
+            if (choice === 'Update Employee Role') {
+                updateRole();
             }
 
-            if (answer.tableChoice === 'Quit') {
+            if (choice === 'Quit') {
                 
             }
         }
@@ -51,3 +51,86 @@ var optionsList = () => {
 }
 
 optionsList();
+
+var addDepartment = () => {
+
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'department',
+            message: 'What is the name of the department?'
+        }
+    ]);
+
+}
+
+var addRole = () => {
+
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'roleName',
+            message: 'What is the name of the role?'
+        },
+        {
+            type: 'input',
+            name: 'roleSalary',
+            message: 'What is the salary of the role?'
+        },
+        {
+            type: 'list',
+            name: 'roleSalary',
+            message: 'What department does the role belong to?',
+            choices: ['[insert array here somehow]']
+        }
+    ]);
+
+}
+
+var addEmployee = () => {
+
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'fisrt',
+            message: 'What is the first name of the employee?'
+        },
+        {
+            type: 'input',
+            name: 'last',
+            message: 'What is the last name of the employee?'
+        },
+        {
+            type: 'list',
+            name: 'roleEmploy',
+            message: 'What is the role of the employee?',
+            choices: ['[insert array here somehow]']
+        },
+        {
+            type: 'list',
+            name: 'managerEmploy',
+            message: 'Who is the manager of the employee?',
+            choices: ['None', '[insert array here somehow]']
+        }
+    ]);
+
+}
+
+var updateRole = () => {
+
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'updateEmployee',
+            message: 'Which employees role would you like to update?',
+            choices: ['[array]']
+        },
+        {
+            type: 'list',
+            name: 'updateRole',
+            message: 'Which role would you like to assign the employee?',
+            choices: ['[array]']
+        }
+    ]);
+
+}
