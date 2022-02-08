f1b6b64c85134ecac387b19b3ef11bd3603b972d
Coder J <buildingguy@sbcglobal.net>
2022-02-07 19:47:16 -0800

    set up functions for department routes to be put in prompts

--- routes/apiRoutes/departmentRoutes.js
+++ routes/apiRoutes/departmentRoutes.js
@@ -3,47 +3,55 @@ const db = require('../../localhost/connection');
 const router = express.Router();
 const inputCheck = require('../../utils/inputCheck');
 
-// view all departments
-router.get('/departments', (req, res) => {
-    const sql = `SELECT * FROM DEPARTMENTS`;
+var department = {};
 
-    db.query(sql, (err, rows) => {
-        if (err) {
-            res.status(500).json({ error: err.message });
-            return;
-        }
-        res.json({
-            message: 'success',
-            data: rows
+department.view = function () {
+    // view all departments
+    router.get('/departments', (req, res) => {
+        const sql = `SELECT * FROM DEPARTMENTS`;
+
+        db.query(sql, (err, rows) => {
+            if (err) {
+                res.status(500).json({ error: err.message });
+                return;
+            }
+            res.json({
+                message: 'success',
+                data: rows
+            });
         });
     });
-});
+}
 
-// create a department
-router.post('department', ({ body }, res) => {
-    const errors = inputCheck(
-        body,
-        'name',
-    );
-    if (errors) {
-        res.status(400).json({ error: errors });
-        return;
-    }
-
-    const sql = `INSERT INTO candidates (name) VALUES (?)`;
-    const params = [
-        body.name,
-        body.id
-    ];
-
-    db.query(sql, params, (err, result) => {
-        if (err) {
-            res.status(400).json({ error: err.message});
+department.create = function () {
+    // create a department
+    router.post('department', ({ body }, res) => {
+        const errors = inputCheck(
+            body,
+            'name',
+        );
+        if (errors) {
+            res.status(400).json({ error: errors });
             return;
         }
-        res.json({
-            message: 'success',
-            data: body
+
+        const sql = `INSERT INTO candidates (name) VALUES (?)`;
+        const params = [
+            body.name,
+            body.id
+        ];
+
+        db.query(sql, params, (err, result) => {
+            if (err) {
+                res.status(400).json({ error: err.message});
+                return;
+            }
+            res.json({
+                message: 'success',
+                data: body
+            });
         });
     });
-});
+}
+
+module.exports = department;
