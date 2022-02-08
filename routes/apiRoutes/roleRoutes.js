2a9802f7222d4c689a3e89407d0b0cb14c820f6b
Coder J <buildingguy@sbcglobal.net>
2022-02-07 19:43:13 -0800

    set up routes for department

--- routes/apiRoutes/roleRoutes.js
+++ routes/apiRoutes/roleRoutes.js
@@ -1,3 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const localhost = require('../../localhost/connection');
+const db = require('../../localhost/connection');
+const inputCheck = require('../../utils/inputCheck');
+
