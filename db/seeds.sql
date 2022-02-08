f50816e2361cc7976fb201be726a0fa5c6e2e786
Coder J <buildingguy@sbcglobal.net>
2022-02-07 18:12:50 -0800

    add more rows to table

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,17 +1,31 @@
 INSERT INTO department
     (name)
 VALUES
-    ('Engineering');
+    ('Engineering'),
+    ('Finance'),
+    ('Legal'),
+    ('Sales'),
+    ('Administration');
 
 INSERT INTO role
-    (title, salary)
+    (title, salary, department_id)
 VALUES
-    ('Software Engineer', '90000');
+    ('Software Engineer', '90000', '1'),
+    ('Accountant', '75000', '2'),
+    ('Attorney', '230000', '3'),
+    ('Salesperson', '110000', '4'),
+    ('Secretary', '50000', '5');
 
 INSERT INTO employee
-    (first_name, last_name)
+    (first_name, last_name, role_id)
 VALUES
-    ('Leonardo', 'Da Vinci');
+    ('Leonardo', 'Da Vinci', '1'),
+    ('Mona', 'Lisa', '2'),
+    ('Pablo', 'Picasso', '3'),
+    ('Niccolo', 'Machiavelli', '4'),
+      ('Alexandria', 'Victoria', '5');
+
+
 
 
 
