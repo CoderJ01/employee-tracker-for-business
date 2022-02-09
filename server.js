d27f99c932de58354b17119797229ac79c3bae88
Coder J <buildingguy@sbcglobal.net>
2022-02-08 19:09:35 -0800

    overhaul: change file arrangement (for do over)

--- server.js
+++ server.js
@@ -1,27 +1,160 @@
-const express = require('express');
-const localhost = require('./localhost/connection');
-const { department, role, employee } = require('./routes/apiRoutes');
-const promptUser = require('./inquirer');
+const inquirer = require('inquirer');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+var optionsList = () => {
 
-app.use(express.urlencoded({ extended: true}));
-app.use(expressjson());
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'tableChoice',
+            message: 'What would you like to do?',
+            choices: ['View All Departments','View All Roles','View All Employees','Add Department',
+            'Add Role','Add Employee','Update Employee Role', 'Quit']
+        }
+    ])
+    .then(
+        answer => {
+            var choice = answer.tableChoice;
 
-// app.use('api', apiRoutes);
-app.use('api', department);
-app.use('api', role);
-app.use('api', employee);
+            if (choice === 'View All Departments') {
+              
+            }
 
-app.use((req, res) => {
-    res.status(404).end();
-});
+            if (choice === 'View All Roles') {
+             
+            }
 
-localhost.connect(err => {
-    if (err) throw err;
-    console.log('Database connected.');
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-});
+            if (choice === 'View All Employees') {
+               
+            }
+
+            if (choice === 'Add Department') {
+        
+            }
+
+            if (choice === 'Add Role') {
+           
+            }
+
+            if (choice === 'Add Employee') {
+           
+            }
+
+            if (choice === 'Update Employee Role') {
+           
+            }
+
+            if (choice === 'Quit') {
+                
+            }
+        }
+    );
+}
+
+optionsList();
+
+var addDepartment = () => {
+
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'department',
+            message: 'What is the name of the department?'
+        }
+    ]).then(
+        answer => {
+            if (answer.department) {
+                department.create();
+            }
+        }
+    );
+
+}
+
+var addRole = () => {
+
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'roleName',
+            message: 'What is the name of the role?'
+        },
+        {
+            type: 'input',
+            name: 'roleSalary',
+            message: 'What is the salary of the role?'
+        },
+        {
+            type: 'list',
+            name: 'roleDepartment',
+            message: 'What department does the role belong to?',
+            choices: ['[insert array here somehow]']
+        }
+    ]).then(
+        answer => {
+            if (answer.roleName && answer.roleSalary && answer.roleDepartment) {
+                role.create();
+            }
+        }
+    );
+
+}
+
+var addEmployee = () => {
+
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'first',
+            message: 'What is the first name of the employee?'
+        },
+        {
+            type: 'input',
+            name: 'last',
+            message: 'What is the last name of the employee?'
+        },
+        {
+            type: 'list',
+            name: 'roleEmploy',
+            message: 'What is the role of the employee?',
+            choices: ['[insert array here somehow]']
+        },
+        {
+            type: 'list',
+            name: 'managerEmploy',
+            message: 'Who is the manager of the employee?',
+            choices: ['None', '[insert array here somehow]']
+        }
+    ]).then(
+        answer => {
+            if (answer.first && answer.last && answer.roleEmploy && answer.managerEmploy) {
+                employee.create();
+            }
+        }
+    );
+
+}
+
+var updateRole = () => {
+
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'updateEmployee',
+            message: 'Which employees role would you like to update?',
+            choices: ['[array]']
+        },
+        {
+            type: 'list',
+            name: 'updateRole',
+            message: 'Which role would you like to assign the employee?',
+            choices: ['[array]']
+        }
+    ]).then(
+        answer => {
+            if (answer.updateEmployee && answer.updateRole) {
+                employee.update();
+            }
+        }
+    );
+
+}
