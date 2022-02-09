6b5efda003026a0cc87b67533301d8ffdc9f7b23
Coder J <buildingguy@sbcglobal.net>
2022-02-08 19:01:40 -0800

    overhaul: get rid of functions

--- routes/apiRoutes/departmentRoutes.js
+++ routes/apiRoutes/departmentRoutes.js
@@ -6,52 +6,11 @@ const inputCheck = require('../../utils/inputCheck');
 var department = {};
 
 department.view = function () {
-    // view all departments
-    router.get('/departments', (req, res) => {
-        const sql = `SELECT * FROM department`;
-
-        db.query(sql, (err, rows) => {
-            if (err) {
-                res.status(500).json({ error: err.message });
-                return;
-            }
-            res.json({
-                message: 'success',
-                data: rows
-            });
-        });
-    });
+    
 }
 
 department.create = function () {
-    // create a department
-    router.post('/department', ({ body }, res) => {
-        const errors = inputCheck(
-            body,
-            'name',
-        );
-        if (errors) {
-            res.status(400).json({ error: errors });
-            return;
-        }
-
-        const sql = `INSERT INTO candidates (name) VALUES (?)`;
-        const params = [
-            body.name,
-            body.id
-        ];
-
-        db.query(sql, params, (err, result) => {
-            if (err) {
-                res.status(400).json({ error: err.message });
-                return;
-            }
-            res.json({
-                message: 'success',
-                data: body
-            });
-        });
-    });
+    
 }
 
-module.exports = department;
+
--- routes/apiRoutes/employeeRoutes.js
+++ routes/apiRoutes/employeeRoutes.js
@@ -6,86 +6,15 @@ const inputCheck = require('../../utils/inputCheck');
 var employee = {};
 
 employee.view = function () {
-    // view all employees
-    router.get('/employees', (req, res) => {
-        const sql = `SELECT * FROM employee`;
-
-        db.query(sql, (err, rows) => {
-            if (err) {
-                res.status(500).json({ error: err.message });
-                return;
-            }
-            res.json({
-                message: 'success',
-                data: rows
-            });
-        });
-    });
+    
 }
 
 employee.create = function () {
-    // create an employee
-    router.post('/employee', ({ body }, res) => {
-        const errors = inputCheck(
-            body,
-            'first_name',
-            'last_name'
-        );
-        if (errors) {
-            res.status(400).json({ error: errors });
-            return;
-        }
-
-        const sql = `INSERT INTO employee (first_name, last_name) VALUES (?, ?)`;
-        const params = [
-            body.first_name,
-            body.last_name,
-            body.role_id
-        ];
-
-        db.query(sql, params, (err, result) => {
-            if (err) {
-                res.status(400).json({ error: err.message });
-                return;
-            }
-            res.json({
-                message: 'success',
-                data: body
-            });
-        });
-    });
+    
 }
 
 employee.update = function () {
-    // update an employee's role
-    router.put('/employee/:id', (req, res) => {
-        const errors = inputCheck(req.body, 'role_id');
-        if (errors) {
-            res.status(400).json({ error: errors });
-            return;
-        }
-        const sql = `UPDATE employee SET role_id = ?
-                     WHERE id = ?`;
-        const params = [req.body.party_id, req.params.id];
-
-        db.query(sql, params, (err, result) => {
-            if (err) {
-                res.status(400).json({ error: err.message });
-            }
-            else if (!result.affectedRows) {
-                res.json({
-                    message: 'Employee not found'
-                });
-            }
-            else {
-                res.json({
-                    message: 'success',
-                    data: req.body,
-                    changes: result.affectRows
-                });
-            }
-        });
-    });
+    
 }
 
 module.exports = employee;
